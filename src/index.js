b5ce5f6fcc199b464b6b84bc134d4e4d50d0c811
Kseniya <55946556+kseniya1706@users.noreply.github.com>
2019-10-05 23:27:48 +0300

    Changes to be committed: 	modified:   package-lock.json 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -4,4 +4,17 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
-};
+let result = 0;
+
+preferences.forEach ((e, i) => {
+  let firstElement = i;
+  let secondElement = preferences[i] - 1;
+  let thirdElement = preferences[secondElement] - 1;
+  if (preferences[thirdElement] - 1 === firstElement)
+  {
+    ++result;
+  }
+}
+);
+return Math.floor(result/3);
+}
